8ac4a750b36dc0277057a8da7ba7ba99385d2ba7
Inés Sanz <inessanzm@hotmail.es>
2025-01-11 12:12:04 +0000

    feat: base archive created

---
+++ src/js/component/home.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+//include images into your bundle
+
+
+//create your first component
+const Home = () => {
+	return (
+		<div className="text-center">
+			<h1>Home!</h1>
+			
+		</div>
+	);
+};
+
+export default Home;
---
+++ webpack.config.js
@@ -0,0 +1,92 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
+const ErrorOverlayPlugin = require('error-overlay-webpack-plugin');
+
+const port = 3000;
+let publicUrl = `ws://localhost:${port}/ws`;
+
+// For Gitpod workspace URL configuration
+if (process.env.GITPOD_WORKSPACE_URL) {
+  const [schema, host] = process.env.GITPOD_WORKSPACE_URL.split('://');
+  publicUrl = `wss://${port}-${host}/ws`;
+}
+
+// For Codespaces workspace URL configuration
+if (process.env.CODESPACE_NAME) {
+  publicUrl = `wss://${process.env.CODESPACE_NAME}-${port}.app.github.dev/ws`;
+}
+
+module.exports = {
+  entry: [
+    './src/js/index.js'
+  ],
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public'),
+    publicPath: '/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+      {
+        test: /\.(css)$/, 
+        use: [
+          'style-loader', 
+          'css-loader'
+        ]
+      }, // CSS files
+      { 
+        test: /\.(png|svg|jpg|gif)$/, 
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]',
+            outputPath: 'images/', // Place images in 'images' folder
+          }
+        }
+      }, // Images
+      { 
+        test: /\.(woff($|\?)|\.woff2($|\?)|\.ttf($|\?)|\.eot($|\?)|\.svg($|\?))/, 
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]',
+            outputPath: 'fonts/', // Place fonts in 'fonts' folder
+          }
+        }
+      } // Fonts
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+  devtool: "source-map",
+  devServer: {
+    port,
+    hot: true,
+    allowedHosts: "all",
+    historyApiFallback: true,
+    static: {
+      directory: path.resolve(__dirname, "public"), // Serve from 'public' directory
+    },
+    client: {
+      webSocketURL: publicUrl
+    },
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new ESLintPlugin({
+      files: path.resolve(__dirname, "src"),
+    }),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'template.html'), // Ensure template path is correct
+      inject: 'body', // Inject JS at the end of the body
+    }),
+  ]
+};
